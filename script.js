efada509d29ad6dbca4dc0b171a90baf49a35324
Cecilia Strüber <strueber.cecilia@googlemail.com>
2021-10-08 16:27:43 +0200

    Undefined

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// function subMit(ev) {
+//   ev.preventDefault;
+//   const nameValue = "test";
+//   const passValue = document.getElementById("pass1").value;
+//   const confpassValue = document.getElementById("pass2").value;
+//   if (typeof nameValue !== "string") {
+//     window.alert("Please re-enter your name");
+//   } else if (passValue !== confpassValue) {
+//     window.alert("Passwords do not match!");
+//   }
+// }
