83b61ae1e731765fe176bb0cff8645d1408182ab
Cecilia Strüber <strueber.cecilia@googlemail.com>
2021-10-11 07:41:59 +0200

    Testing for Equal Check

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
 "use strict";
-// Funktion für
-// function myFunction() {
-//   const inpObj = document.querySelector("#pass1");
-//   const inpObjValue = inpObj.value;
-//   console.log(inpObjValue);
-//   const inpObj2 = document.querySelector("#pass2");
-//   const inpObj2Value = inpObj2.value;
-//   console.log(inpObj2Value);
-// }
-
-// Passwort im Klartext anzeigen
-// Weitere Vorgehensweise theoretisch -->document.getElementById("Button").disabled = true; setzen
-const showHiddenButton = document.getElementById("btn");
-showHiddenButton.addEventListener("click", myFunction);
-
+//Function for the equal check
 function myFunction() {
-  if (document.getElementById("btn").disabled == false) {
-    document.getElementById("Passwort").type = "text";
-    showHiddenButton.innerText = "Hidden Password";
-  } else {
-    document.getElementById("Passwort").type = "password";
-    showHiddenButton.innerText = "Show Password";
+  const inpObj = document.querySelector("#pass1");
+  const inpObjValue = inpObj.value;
+  console.log(inpObjValue);
+  const inpObj2 = document.querySelector("#pass2");
+  const inpObj2Value = inpObj2.value;
+  console.log(inpObj2Value);
+  if (inpObj != inpObj2) {
+    alert("password has to be equal!");
+    document.getElementById("pass1").focus();
+    return false;
   }
+  return true;
 }
+
+// Showing Password in Cleartext
+// Weitere Vorgehensweise theoretisch -->document.getElementById("Button").disabled = true; setzen
+// const showHiddenButton = document.getElementById("btn");
+// showHiddenButton.addEventListener("click", myFunction);
+
+// function myFunction() {
+//   if (document.getElementById("btn").disabled == false) {
+//     document.getElementById("Passwort").type = "text";
+//     showHiddenButton.innerText = "Hidden Password";
+//   } else {
+//     document.getElementById("Passwort").type = "password";
+//     showHiddenButton.innerText = "Show Password";
+//   }
+// }
