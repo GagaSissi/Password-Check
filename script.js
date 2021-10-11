a4c18915e8a8d44b81c1642f5bc8362fb3f73e79
Cecilia Strüber <strueber.cecilia@googlemail.com>
2021-10-11 06:28:57 +0200

    test: Button false/true

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 "use strict";
+// Funktion für
+// function myFunction() {
+//   const inpObj = document.querySelector("#pass1");
+//   const inpObjValue = inpObj.value;
+//   console.log(inpObjValue);
+//   const inpObj2 = document.querySelector("#pass2");
+//   const inpObj2Value = inpObj2.value;
+//   console.log(inpObj2Value);
+// }
+
+// Passwort im Klartext anzeigen
+// Weitere Vorgehensweise theoretisch -->document.getElementById("Button").disabled = true; setzen
+const showHiddenButton = document.getElementById("btn");
+showHiddenButton.addEventListener("click", myFunction);
 
 function myFunction() {
-  const inpObj = document.querySelector("#pass1");
-  const inpObjValue = inpObj.value;
-  console.log(inpObjValue);
-  const inpObj2 = document.querySelector("#pass2");
-  const inpObj2Value = inpObj2.value;
-  console.log(inpObj2Value);
+  if (document.getElementById("btn").disabled == false) {
+    document.getElementById("Passwort").type = "text";
+    showHiddenButton.innerText = "Hidden Password";
+  } else {
+    document.getElementById("Passwort").type = "password";
+    showHiddenButton.innerText = "Show Password";
+  }
 }
